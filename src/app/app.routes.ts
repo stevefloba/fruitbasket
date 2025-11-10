913026c901453b4aaaf743a5f85909f73248e539
Stefan Flosbach <stefanflosbach@t-online.de>
2025-11-10 14:13:36 +0100

    Grundstruktur erstellt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Fruitlist } from './fruitlist/fruitlist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Fruitlist },
+];
